e9bd81dc405e17d975486c7c4a0f9e8a77bf609a
Alex Muiruri <legittpayout@gmail.com>
2026-06-17 09:26:54 +0300

    Feat: Added suspense in shop page

--- src/app/(e-commerce)/shop/page.tsx
+++ src/app/(e-commerce)/shop/page.tsx
@@ -1,12 +1,15 @@
 import { getProducts } from "@/src/action/productController"
 import { ShopPage } from "@/src/components/sections/ShopPage"
+import { Suspense } from "react"
 
 const HTWContactApp = async () => {
     const products = await getProducts()
 
     return (
         <main>
-            <ShopPage products={products} />
+            <Suspense fallback={<div></div>}>
+                <ShopPage products={products} />
+            </Suspense>
         </main>
     )
 }
